117295b32b34fa52cc97208cc8bd7349d1cd1873
Natalia Linkova <linkova1109@yandex.ru>
2023-06-27 13:07:22 +0500

    style: :lipstick: game card style

--- src/components/GameCardComponent.tsx
+++ src/components/GameCardComponent.tsx
@@ -1,47 +1,81 @@
 import * as React from 'react';
 import { Dimensions, Image, ImageSourcePropType, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { } from 'react-native-paper';
 import { GameCard } from '../types';
 
 interface GameCardProps {
   item: GameCard;
   onPress: (item: GameCard) => void;
 }
+
 const { width } = Dimensions.get('window');
 const cardWidth = width / 5;
 
 const GameCardComponent: React.FC<GameCardProps> = ({ item, onPress }) => {
   const imageSource: ImageSourcePropType = { uri: item.background_image };
+
   return (
-    <TouchableOpacity onPress={() => onPress(item)}>
-      <View>
-        <Text>{item.name}</Text>
-        <View>
-          <Text>{item.rating}</Text>
-        </View>
+    <TouchableOpacity onPress={() => onPress(item)} style={styles.gameCardContainer}>
+      <View style={styles.cardContent}>
         <Image style={styles.gameImage} source={imageSource} />
-        <View>
-          <Pressable>
-            <Text>Cancel</Text>
-          </Pressable>
-          <Pressable>
-            <Text>OK</Text>
-          </Pressable>
+        <View style={styles.cardDetails}>
+          <Text style={styles.cardTitle}>{item.name}</Text>
+          <Text style={styles.cardRating}>Rating: {item.rating}</Text>
         </View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Cancel</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>OK</Text>
+        </Pressable>
       </View>
     </TouchableOpacity>
   );
-}
+};
 
 export default GameCardComponent;
 
 const styles = StyleSheet.create({
-  gameCardCntainer: {
+  gameCardContainer: {
     width: cardWidth,
     alignItems: 'center',
+    marginBottom: 16,
+  },
+  cardContent: {
+    borderWidth: 1,
+    borderRadius: 8,
+    overflow: 'hidden',
+    marginBottom: 8,
   },
   gameImage: {
     width: cardWidth,
     height: cardWidth,
   },
-})
+  cardDetails: {
+    padding: 8,
+  },
+  cardTitle: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    marginBottom: 4,
+  },
+  cardRating: {
+    fontSize: 10,
+    color: 'gray',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  button: {
+    backgroundColor: 'lightgray',
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    borderRadius: 4,
+    marginHorizontal: 4,
+  },
+  buttonText: {
+    fontSize: 10,
+    fontWeight: 'bold',
+  },
+});
